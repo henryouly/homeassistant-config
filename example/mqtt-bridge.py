571c5161857bf11c8b4590b21a51ff2eb213946f
Henry Ou <henryouly@gmail.com>
2017-11-09 22:46:02 -0800

    some clean up

--- example/mqtt-bridge.py
+++ example/mqtt-bridge.py
@@ -13,12 +13,6 @@ PORT = 15382
 client = Client(client_id="mqtt_bridge", clean_session=False)
 subscriptions = []
 callback = ""
-history = {}
-
-class MainHandler(RequestHandler):
-  def get(self):
-    self.write("This message comes from Tornado ^_^")
-
 
 class PushHandler(RequestHandler):
   """
@@ -27,12 +21,11 @@ class PushHandler(RequestHandler):
   * @param  {String}  req.body.value Value of device (e.g. "on")
   """
   def post(self):
-    logging.info("/push: {0}".format(self.request.body))
     data = json_decode(self.request.body)
-    name = data["name"]
-    type = data["type"]
+    device = data["name"]
+    property = data["type"]
     value = data["value"]
-    topic = "/".join([PREFACE, name, type])
+    topic = "/".join([PREFACE, device, property])
     logging.info("Incoming message from SmartThings: {0} = {1}".format(topic, value))
     res, mid = client.publish(topic, value, retain=True)
     if res == MQTT_ERR_SUCCESS:
@@ -40,7 +33,6 @@ class PushHandler(RequestHandler):
 
 class SubscribeHandler(RequestHandler):
   def post(self):
-    logging.info("/subscribe: {0}".format(self.request.body))
     data = json_decode(self.request.body)
     global subscriptions
     subscriptions = []
@@ -53,23 +45,23 @@ class SubscribeHandler(RequestHandler):
     callback = data["callback"]
     logging.info("Received callback: {0}".format(callback))
       
-    logging.info("Subscribing to {0}".format(",".join(subscriptions)))
+    logging.info("Subscribing to {0}".format(subscriptions))
     for topic in subscriptions:
       client.subscribe(topic)
     self.write({"status": "OK"})
 
 def on_message(client, userdata, message):
-  logging.info("Incoming message from MQTT: {0} = {1}".format(message.topic, message.payload.decode()))
-  pieces = message.topic.strip().split('/') 
-  device = pieces[1]
-  property = pieces[2]
+  topic = message.topic
+  value = message.payload.decode()
+  logging.info("Incoming message from MQTT: {0} = {1}".format(topic, value))
+  _, device, property = topic.strip().split('/') 
   topicState = "/".join([PREFACE, device, property])
   topicSwitchState = "/".join([PREFACE, device, "switch"])
   topicLevelCommand = "/".join([PREFACE, device, "level"])
   
   logging.info("Sending message to {0}".format(callback))
   import requests
-  res = requests.post('http://' + callback, json={"name": device, "type": property, "value": message.payload.decode().strip()})
+  res = requests.post('http://' + callback, json={"name": device, "type": property, "value": value.strip()})
   if res.status_code >= 300:
     logging.error("Error from SmartThings Hub: {0}".format(res.status_code))
 
@@ -85,17 +77,13 @@ def connectToMQTT(host):
   client.connect(host, port=1883)
   client.loop_start()
 
-def make_app():
-  return Application([
-      (r"/", MainHandler),
-      (r"/push", PushHandler),
-      (r"/subscribe", SubscribeHandler),
-  ])
-
 if __name__ == "__main__":
   logging.basicConfig(level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S',
                       format='%(asctime)-15s - [%(levelname)s] %(module)s: %(message)s',)
-  app = make_app()
+  app = Application([
+      (r"/push", PushHandler),
+      (r"/subscribe", SubscribeHandler),
+  ])
   app.listen(PORT)
   logging.info("Listening at http://localhost:{0}".format(PORT))
   connectToMQTT("localhost")
